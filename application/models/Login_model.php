9ec855f4568abaa8c09b7fe8b5e6f06c55f7fced
Guillermo Villanueva <gvilla@justicia.salta>
2020-02-17 12:59:53 -0300

    pj 20200217

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -2,9 +2,9 @@
 class Login_model extends CI_Model{
  
   function validate($email,$password){
-    $this->db->where('us_email',$email);
-    $this->db->where('us_password',$password);
-    $result = $this->db->get('users',1);
+    $this->db->where('soc_email',$email);
+    $this->db->where('soc_password',$password);
+    $result = $this->db->get('socios',1);
     return $result;
   }
  
